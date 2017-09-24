f737a418062abb7db65068f40199a70d385d2627
Charles T. Betz <char@dm-academy.com>
2017-09-24 12:59:28 -0500

    docker ho

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,7 @@
 Vagrant.configure("2") do |config|
 
  config.vm.box = "ubuntu/xenial64"
+ config.vm.network "forwarded_port", guest: 3000, host: 3000, host_ip: "127.0.0.1"
  config.vm.provision :shell, path: "./install-docker.sh"
 
 end
--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y curl
+apt-get install -y tree
+
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
+sudo usermod -aG docker $USER
+
+# apt-get install -y docker-compose
+#
+# apt-get install -y nodejs npm
+#
+# mkdir /home/ubuntu/app1
+#
+# cp -r /vagrant/app1-rsrc/* /home/ubuntu/app1/
+# cd /home/ubuntu/app1
+# npm install express
+# # npm install mocha --save
+# # npm install chai --save # do these auto-install on first run??
+#
+# echo "starting node app"
+# nodejs app.js &
+# echo "started, perhaps"
 
 # to test, sudo docker run hello-world
 # and docker run -it ubuntu bash
+
+# write node server that serves HTML
+# wrap in mocha
+# dockerize it
+# script it
