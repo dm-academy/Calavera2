e04cea1a03b41b261dcf2f9280e29f14d2e9998b
CharlesTBetz <char@erp4it.com>
2017-10-15 13:41:20 -0500

    hombros fix

--- hombros/start.sh
+++ hombros/start.sh
@@ -1,2 +1,4 @@
- docker run -p 8080:8080 --env JAVA_OPTS="-Dhudson.Main.development=true -Djenkins.install.runSetupWizard=false"  hombros
+docker build .
+
+docker run -p 8080:8080 --env JAVA_OPTS="-Dhudson.Main.development=true -Djenkins.install.runSetupWizard=false"  hombros
 
