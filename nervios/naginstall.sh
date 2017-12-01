7e16d7d8780574888ad27114c28b32e1e073806d
jlmoldan <jlmoldan@stthomas.edu>
2017-12-01 15:51:46 -0600

    Update naginstall.sh

--- nervios/naginstall.sh
+++ nervios/naginstall.sh
@@ -6,33 +6,14 @@
 
 ### pre work
 apt-get update
+apt-get upgrade -y
 apt-get install --yes vim net-tools apt-utils
 
 ########################################################
 #### Installation Starts ######
 ########################################################
 
-chk_user()
-{
-        if [ $(whoami) != "root" ]
-        then
-                echo -e "\n\t\033[44;37;5m###### WARNING ######\033[0m"
-                echo -e "\t${txtylw}${txtbld}Sorry ${txtgrn}$(whoami)${txtrst}${txtylw}${txtbld}, you must login as root user to run this script.${txtrst}"
-                echo -e "\t${txtylw}${txtbld}Please become root user using 'sudo -s' and try again.${txtrst}"
-                echo -e
-                echo -e "\t${txtred}${txtbld}Quitting Installer.....${txtrst}\n"
-                sleep 3
-        exit 1
-        fi
-}
-
-### Ubuntu/Debian type servers. ####
-chk_user
-# Remove crap to get Dockerfile to work
-#rm -rf /etc/init.d/apache2
-#rm -rf /etc/init.d/nagios3
-                                apt-get update
-                                apt-get upgrade -y
+                                                              
                                 apt-get -y  install apache2 build-essential wget perl openssl supervisor
                                 apt-get -y  install nagios-plugins
                                 ##apt-get -y  install nagios3
@@ -51,10 +32,10 @@ chk_user
                                 chmod -R g+wx /var/lib/nagios3/
 
                                 /etc/init.d/nagios3 restart
-
+                                sleep 2
                                 /etc/init.d/apache2 restart
                                 sleep 2
-                                IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+                               
                                 echo -e "\n\tNow Nagios is ready to be used via: http://$IP/nagios3"
 
 
