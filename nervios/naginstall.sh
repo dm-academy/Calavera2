ac32e2580b73fe1fc3886a4c2a35eaf9384c119d
jlmoldan <jlmoldan@stthomas.edu>
2017-11-30 18:26:49 -0600

    Update naginstall.sh

--- nervios/naginstall.sh
+++ nervios/naginstall.sh
@@ -41,6 +41,13 @@ chk_user
                                 htpasswd -cb /etc/nagios3/htpasswd.users nagiosadmin password
                                 cp Calavera2/nervios/*.cfg /etc/nagios3/conf.d/
                                 
+                                echo "NAGIOS DONE ##########################################################################"
+                                 echo "NAGIOS DONE ##########################################################################"
+                                  echo "NAGIOS DONE ##########################################################################"
+                                   echo "NAGIOS DONE ##########################################################################"
+                                    echo "NAGIOS DONE ##########################################################################"
+                                     echo "NAGIOS DONE ##########################################################################"
+                                     
                                 apt-get -y  install nagios-plugins-basic nagios-plugins-extra nagios-snmp-plugins nagios-nrpe-plugin nagios3-core nagios-plugins-standard nagios3-cgi nagios-plugins-contrib
                                 apt-get -y  install libssl-dev openssh-server openssh-client ntpdate snmp smbclient libldap-2.4-2 libldap2-dev  unzip
                                 sed -i 's/check_external_commands=0/check_external_commands=1/g' /etc/nagios3/nagios.cfg
