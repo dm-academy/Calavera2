f279c3ca6cd8e2ad21107ce41fd09f59b0f4ff10
Sidi Cheikh <chei8547@stthomas.edu>
2017-12-08 18:23:34 -0600

    leave artifactory running on the default port 8081

--- espina/startup.sh
+++ espina/startup.sh
@@ -1,5 +1,5 @@
 docker build . -t espina
 #docker run -it -p 8889:8081 espina
-docker run  -td --net cala01 -p 8889:8081  --ip 203.0.113.42 espina
+docker run  -td --net cala01   --ip 203.0.113.42 espina
 
 
