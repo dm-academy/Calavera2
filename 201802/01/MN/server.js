ad7fbcffb8023ec398d1ad9f034e2c1b93e6ba1b
Marinel Farinha-Guevara <fari5273@stthomas.edu>
2018-04-10 19:58:31 -0500

    adding our group files

---
+++ 201802/01/MN/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use('/public', express.static((__dirname+'/public')));
+
+// Constants
+const PORT = 3000;
+//const HOST = '0.0.0.0';
+
+//ROUTE
+// app.get('/', (req, res) => {
+//  res.send(__dirname+'');
+// });
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname+'/mn_attractions.html');
+});
+
+
+
+//app.listen(PORT, HOST);
+//console.log(`Running on http://${HOST}:${PORT}`);
+
+app.listen(PORT,function(){
+	console.log("Server running and listening in port " + PORT);
+});
