7b51345e99c50d620c8ea47cf5f4e4facb463b04
paddytay <paddytaytsechi@gmail.com>
2018-04-27 20:34:49 -0500

    project group 03

--- 201802/03/docker2/server.js
+++ 201802/03/docker2/server.js
@@ -9,8 +9,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.sendFile(__dirname+'/welcomehome.html');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, function(){
+console.log(`Server listening on port` + PORT);
+});
